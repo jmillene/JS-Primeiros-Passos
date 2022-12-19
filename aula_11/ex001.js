2cbb27cbf1e15be12c98b23df487b3d7776f231f
jessica <jmillene12@gmail.com>
2022-12-18 21:43:51 -0300

    treinando o DOM

---
+++ aula_11/ex001.js
@@ -0,0 +1,6 @@
+const velocidade = 60.5;
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+if (velocidade > 60) {
+  console.log("Você ultrapssou a velocidade permitida.MULTADO");
+}
+console.log("Dirija sempre com sinto de segurança");
