17c8cc9fca36b763e5e504918f89f589ecc5c9a2
jessica <jmillene12@gmail.com>
2022-12-21 19:29:56 -0300

    exercicios

---
+++ JavaScriptBasicoTrybe/aula_2/array.js
@@ -0,0 +1,29 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// tasksList.push("Pão de Queijo")
+// console.log(tasksList.length);
+// console.log(tasksList);
+
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Reunião');
+console.log(indexOfTask);
+
+// 1
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+console.log(beasts.indexOf('bison'));
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1];
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push('Contato')
+console.log(menu);
---
+++ JavaScriptBasicoTrybe/aula_2/for.js
@@ -0,0 +1,13 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for(let index = 0; index < groceryList.length; index +=1){
+  console.log(groceryList[index]);
+}
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let nome of  names){
+  console.log(nome);
+}
+
+
