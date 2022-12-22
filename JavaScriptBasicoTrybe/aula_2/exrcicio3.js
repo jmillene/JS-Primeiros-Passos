93d5691e2def0e8cd2202377c57c02cf4aebb79c
Jessica <jmillene12@gmail.com>
2022-12-21 21:53:26 -0300

    exercicio de arrays

---
+++ JavaScriptBasicoTrybe/aula_2/exrcicio3.js
@@ -0,0 +1,17 @@
+// Calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for(let index = 0; index< numbers.length; index +=1){
+soma+=numbers[index]
+}
+const media = soma/numbers.length;
+
+console.log(media);
+
+if(media > 20){
+ console.log('valor maior que 20');   
+}else{
+    console.log('valor menor ou igual a 20');
+}
